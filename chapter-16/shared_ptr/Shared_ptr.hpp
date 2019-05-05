8eb8e8bd1e8e1dd8bd98ad50d132ea28d879de08
JinLingChristopher <jinl1037@hotmail.com>
2019-05-05 21:20:51 +0800

    add operator = to shared_ptr

--- chapter-16/shared_ptr/Shared_ptr.hpp
+++ chapter-16/shared_ptr/Shared_ptr.hpp
@@ -14,7 +14,7 @@ class Shared_ptr {
     using weak_type = std::weak_ptr<T>;
 
 public:
-    constexpr Shared_ptr() noexcept :resource(nullptr), count(new std::size_t(0)) {
+    constexpr Shared_ptr() noexcept :resource(nullptr), counter(nullptr) {
         std::cout << "Shared_ptr()" << std::endl;
     }
 
@@ -22,37 +22,61 @@ public:
         std::cout << "Shared_ptr(std::nullptr)" << std::endl;
     }
 
-    Shared_ptr(element_type* t):resource(t), count(new std::size_t(1)) {
+    Shared_ptr(element_type* t):resource(t), counter(new std::size_t(1)) {
         std::cout << "Shared_ptr(element_type* t)" << std::endl;
     }
 
-    Shared_ptr(const Shared_ptr& other) {
-
+    Shared_ptr(const Shared_ptr& other):resource(other.resource), counter(other.counter) {
+        *counter += 1;
+        std::cout << "Shared_ptr(const Shared_ptr& other)" << std::endl;
     }
 
     Shared_ptr& operator=(const Shared_ptr& other);
 
-    ~Shared_ptr() {
-        std::cout << "~Shared_ptr()" << std::endl;
-        if (0 == use_count()) {
-            delete count;
-            delete resource;
-        } else {
-            *count -= 1;
-        }
-    }
+    ~Shared_ptr();
 
     T* get() {
         return resource;
     }
 
     std::size_t use_count() {
-        return *(this->count);
+        if (counter == nullptr) {
+            return 0;
+        }
+        return *(this->counter);
     }
 
 private:
     element_type* resource;
-    std::size_t* count;
+    std::size_t* counter;
 };
+
+template<class T>
+Shared_ptr<T> &Shared_ptr<T>::operator=(const Shared_ptr &other) {
+    std::cout << "Shared_ptr<T>::operator=(const Shared_ptr &other)" << std::endl;
+    *other.counter += 1;
+    if (counter != nullptr) {
+        if (--*counter == 0) {
+            delete resource;
+            delete counter;
+        }
+    }
+    resource = other.resource;
+    counter = other.counter;
+    return *this;
+}
+
+template<class T>
+Shared_ptr<T>::~Shared_ptr() {
+    std::cout << "~Shared_ptr()" << std::endl;
+    if (counter != nullptr) {
+        *counter -= 1;
+        if (0 == use_count()) {
+            std::cout << "delete old resource and count" << std::endl;
+            delete counter;
+            delete resource;
+        }
+    }
+}
 
 #endif //CPP_PRIMER_Shared_ptr_HPP
--- chapter-16/shared_ptr/test_shared_ptr.cpp
+++ chapter-16/shared_ptr/test_shared_ptr.cpp
@@ -21,11 +21,24 @@ void func1() {
     }
 }
 
+void func2() {
+    Shared_ptr<int> p1;
+    std::cout << p1.use_count() << std::endl;
+
+    Shared_ptr<int> p2(new int(1));
+    std::cout << p2.use_count() << std::endl;
+
+    Shared_ptr<int> p3(p2);
+    std::cout << p2.use_count() << std::endl;
+    std::cout << (p3.use_count() == p2.use_count()) << std::endl;
+}
 
 int main() {
 
-    Shared_ptr<int> p1;
-    Shared_ptr<int> p2(new int(1));
+    Shared_ptr<string> p1(new string("hello"));
+    Shared_ptr<string> p2(p1);
+    Shared_ptr<string> p3;
+    p3 = p1;
 
     return 0;
 }
